28cac6ca6d3cc8aca049cb5ce8667333c8c9d38e
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-01-14 10:22:20 -0800

    added views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.destroy_all
+Review.destroy_all
+
+50.time do |index|
+  product = Product.create!({name: Faker::Superhero.random, origin: Faker::Address.city, cost: Faker::Number.number(digits: 2)})
+  250.times do |index|
+    Review.create!({author: Faker::Name.name, content_body: Faker::Books::Dune.quote, product_id: product_id })
+  end
+end
+
+p "Created #{Product.count} products."
+p "Created #{Review.count} reviews."
